58bca84a9c1dd6ac09c2a001d4ca722a0e31808d
Duc Nguyen <ngmductm@gmail.com>
2025-01-15 10:33:23 -0500

    set up and test run transform for contest ranking pipeline

--- operators/contest_ranking_ops.py
+++ operators/contest_ranking_ops.py
@@ -1,3 +1,4 @@
+import ast
 import pandas as pd
 import requests
 
@@ -12,6 +13,34 @@ def extract_contest_ranking(num_pages):
         # Get response for each page
         response = requests.post(URL, json=contest_ranking_query(i + 1)).json()["data"]["globalRanking"]["rankingNodes"]
         responses.extend(response)
-    file_path = f"{OUTPUT_PATH}/raw/sample_contest_ranking.csv"  # Local file path for sample output data
-    pd.DataFrame(responses).to_csv(file_path, index=False)
-    return file_path
+    output_path = f"{OUTPUT_PATH}/raw/sample_contest_ranking.csv"  # Local file path for sample output data
+    pd.DataFrame(responses).to_csv(output_path, index=False)
+    return output_path
+
+
+def transform_contest_ranking(task_instance):
+    """Processes the raw data and makes necessary changes"""
+    input_path = task_instance.xcom_pull(task_ids="extract_contest_ranking")
+    df = pd.read_csv(input_path)  # Read the raw data
+
+    # Process the ranking column
+    df["ranking"] = df["ranking"].apply(eval)  # Convert from string of list to list
+    df["contest_count"] = df["ranking"].apply(len)  # Number of contest attended
+    df["avg_ranking"] = df["ranking"].apply(lambda x: round(sum(x) / len(x), 2))  # Avg placement across all contests
+
+    # Process the user column
+    df["user"] = df["user"].apply(ast.literal_eval)  # String of dictionary to dictionary
+    df["username"] = df["user"].apply(lambda x: x["username"])
+    df["country"] = df["user"].apply(lambda x: x["profile"]["countryName"])
+
+    df.loc[df["dataRegion"] == "CN", "country"] = "China"  # Fill country for CN users
+    df["country"] = df["country"].fillna("Unknown")
+    df = df.drop(columns=["ranking", "user", "dataRegion"])  # Unnecessary columns
+    df = df.rename(columns={  # Rename columns for readability
+        "currentRating": "rating",
+        "currentGlobalRanking": "global_ranking",
+    })
+
+    output_path = f"{OUTPUT_PATH}/processed/sample_contest_ranking.csv"
+    df.to_csv(output_path, index=False)
+    return output_path
